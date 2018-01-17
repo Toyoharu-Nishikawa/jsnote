195540f6c53baa520256af4184bfcc7ae76a90b6
toyoharu nishikawa <toyohal@email.com>
2018-01-17 23:54:57 +0900

    fit screen by css

--- public/scripts/variable.js
+++ public/scripts/variable.js
@@ -1,2 +1,16 @@
 var importTexts = [];
 var exportText = "";
+var editor = ace.edit('editor');
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setOptions({
+  mode: "ace/mode/javascript",
+  tabSize: 2,
+  useSoftTabs: true
+}); 
+editor.setKeyboardHandler("ace/keyboard/vim");
+editor.setOptions({
+  fontSize: "13pt"
+});
+editor.$blockScrolling = Infinity; 
+
+
