1e0b5bedcc57fcfa64662eb4ff3ae4ada38c48fb
toyoharu nishikawa <toyohal@email.com>
2018-01-24 22:55:58 +0900

    fix parse_csv.js

--- public/sample/native_javascript/parse_csv.js
+++ public/sample/native_javascript/parse_csv.js
@@ -2,12 +2,17 @@
 //without d3 
 //I love native js 
 
+const transpose = A=>A[0].map((k,i,array)=>A.map((v)=>v[i])); //transpose matrix
+const csvParse = csv => csv
+  .split(/\r\n|\n|\r/) //split by line feed codes
+  .filter((k)=>k.match(/\S/)) //remove empty lines
+  .map((k)=>k.trim().replace(/,+/g,",").split(","));//remove white spaces and split by cannma
+
 var sample = " dog, cat, monkey\n 3, 4, 5 \r\n \n 5, 5,5\r 10,8,1 ";
-var csv = importTexts[0].text || sample; //assign import text or sample string to csv
+
+var csv = importTexts.length >0 ? importTexts[0].text : sample; //assign import text or sample string to csv
 console.log(csv);
 
-var data =csv.split(/\r\n|\n|\r/) //split by line feed codes
-  .filter((k)=>k.match(/\S/)) //remove empty lines
-  .map((k)=>k.trim().replace(/,\s+/g,",").split(","));//remove white spaces and split by cannma
-
+var data = csvParse(csv)
 console.log(data);
+console.log(transpose(data))
