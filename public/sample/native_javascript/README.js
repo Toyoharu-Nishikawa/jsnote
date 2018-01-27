3a3b72eed05974613d9952f594a9223b24903619
toyoharu nishikawa <toyohal@email.com>
2018-01-27 17:14:29 +0900

    add READ ME to native javascript sample

---
+++ public/sample/native_javascript/README.js
@@ -0,0 +1,53 @@
+// 3-line-message
+//write here what you wanto to tell the world
+// javascript is what a wonderful language !! i'm lovin' it.
+
+`
+-----------------
+   READ ME
+-----------------
+
+## Usage
+
+####  FUNCTION BUTTON 
+  
+- "run"  
+    runs your code written in the editor and record the code to local storage of WEB browser.
+
+- "read"  
+    read a text file form your local file system  to paste contents to the editor.
+
+- "save"  
+    save a text file as "jsnote.txt" to your local file system.  
+    the default directory is "Downloads". 
+
+- "import"  
+     imports one or more text files to assing the name and the contents of file to array of "importTexts".  
+     Ex.  
+     importTexts[i].filename      : filename  
+     importTexts[i].barefilename  : filename without extention  
+     importTexts[i].ext           : extention of file  
+     importTexts[i].text          : contents of file  
+
+- "export"  
+    exports the value of "exportText" assigned string data as a text file.  
+    Assign string data to "exportText" before clicking the buttion of export.  
+    Ex.  
+    exportText = "welcome to jsnote" //before clicking the button of export
+
+- "sample"  
+    suggests you a lot of sample codes
+
+#### SHORTCUT KEY 
+
+- "Shift + Enter"  
+    is equal to click the button of "run" to run your code written in the editor.
+
+- "Shift + Tab"  
+    is equal to check draw box on or off to show or hide draw area.  
+    The DOM ID of draw area is "drawArea". It includs DOM of "draw".
+ 
+- "Shift + Delete"  
+    clears all words in the editor.
+
+`
