6652497db2f835352fa1641332f5376d71b39613
toyoharu nishikawa <toyohal@email.com>
2018-02-11 01:44:19 +0900

    remove

--- public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import * as jsnote from "./jsnote.mjs"
-import * as if97 from "./node_modules/jsIF97/index.mjs"
-
-window.if97= if97;
-
-jsnote.key.add();
-jsnote.view.initialize();
-jsnote.control.set();
