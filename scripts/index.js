1df6bb6f6057225e5e8624630f30e9fb09b084f6
toyoharu nishikawa <toyohal@email.com>
2018-07-06 22:29:42 +0900

    comment out necoengine

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 import * as jsnote from "./jsnote/index.mjs"
 import * as if97 from "./jsIF97/index.mjs"
 import * as sci from "./sci/index.mjs"
-import {login,engine} from "/necoengine/scripts/necoengine/index.js"
+//import {login,engine} from "/necoengine/scripts/necoengine/index.js"
 
-login.setLoginButton()
-engine.visit()
+//login.setLoginButton()
+//engine.visit()
 
 window.if97= if97
 window.sci=sci 
