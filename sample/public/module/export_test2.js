cf222b71497d366b100a7460c2acbd79187b6ad2
toyoharu nishikawa <toyohal@email.com>
2019-06-22 18:20:32 +0900

    change sample directory

--- sample/public/module/export_test2.js
+++
@@ -1,9 +0,0 @@
-export const func1 = (value) =>{
-  console.log('export_test2.js func1')
-  return value*3
-}
-
-export const func2 = (value) =>{
-  console.log('export_test2.js func2')
-  return value**3
-}
