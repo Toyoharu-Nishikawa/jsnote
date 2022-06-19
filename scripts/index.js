ca9a224f758bd9a97d320a855c1aa5fa1d47ddc8
nishikawa <>
2022-06-19 11:39:43 +0900

    udated sci

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,18 @@ window.if97= if97
 window.sci=sci 
 
 const main = async () =>{
-//  try{
+  try{
     const necoengine = await import("/necoengine/scripts/necoengine/index.js")
     necoengine.login.setLoginButton()
     necoengine.login.visit()
     const workbench = new necoengine.Workbench()
     initialize(workbench)
     window.workbench = workbench
-//  }
-//  catch(e){
-//    console.log("error",e.message)
-//    console.log("necoengine is not alive")
-//  }
+  }
+  catch(e){
+    console.log("error",e.message)
+    console.log("necoengine is not alive")
+  }
 }
 
 main()
